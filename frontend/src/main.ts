6633ec48da69cd453ea997e98161f418c733fb65
Theycallmeki <laxamana.kian13@gmail.com>
2026-03-16 02:18:32 +0800

    Fix: Leaflet api fix

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,8 @@ import { createPinia } from "pinia"
 import App from "./App.vue"
 import router from "./router"
 
+import "leaflet/dist/leaflet.css"
+
 const app = createApp(App)
 
 const pinia = createPinia()
