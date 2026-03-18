f5fc8b72542330470436da1c706641cf2c1d0c17
Theycallmeki <laxamana.kian13@gmail.com>
2026-03-18 22:20:32 +0800

    Refactor: revert to old commit

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,10 +3,6 @@ import { createPinia } from "pinia"
 import App from "./App.vue"
 import router from "./router"
 
-import PrimeVue from "primevue/config"
-import Aura from "@primevue/themes/aura"
-
-import "primeicons/primeicons.css"
 import "leaflet/dist/leaflet.css"
 
 const app = createApp(App)
@@ -15,11 +11,5 @@ const pinia = createPinia()
 
 app.use(pinia)
 app.use(router)
-
-app.use(PrimeVue, {
-  theme: {
-    preset: Aura
-  }
-})
 
 app.mount("#app")
