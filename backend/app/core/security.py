405f8c62ce249cc6557c071691007f0e58bbbd3f
Theycallmeki <laxamana.kian13@gmail.com>
2026-04-02 15:39:03 +0800

    Fix: Security.py

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -15,7 +15,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str):
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
 
 
 def verify_password(plain_password: str, hashed_password: str):
