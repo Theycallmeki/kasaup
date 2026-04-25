f68c4ae881d6b71dbe225a7c8ace6601e00c436f
Theycallmeki <laxamana.kian13@gmail.com>
2026-04-25 19:15:31 +0800

    Fix: Filter design

--- frontend/src/views/customer/components/ServicesFilterPanel.vue
+++ frontend/src/views/customer/components/ServicesFilterPanel.vue
@@ -400,4 +400,15 @@ const maxDistanceModel = computed({
     gap: 28px;
   }
 }
+
+/* Hide number input spinners */
+input[type="number"]::-webkit-outer-spin-button,
+input[type="number"]::-webkit-inner-spin-button {
+  -webkit-appearance: none;
+  margin: 0;
+}
+
+input[type="number"] {
+  -moz-appearance: textfield;
+}
 </style>
